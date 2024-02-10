fbaa2c4b2cd0e1d8e8852f059837b7bbabb5a39d
CodeInWork <fischerp@gmx.de>
2024-02-10 17:16:46 +0100

    minor changes

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -1,45 +1,42 @@
 import pygame
-import os
-from PyQt6 import QtCore, QtGui, QtWidgets
+
 
 class MusicPlayer:
-    def __init__(self, root) -> None:
-        self.root = root
-        self.playlist = root.currentSoundFilesListWidget
+    def __init__(self) -> None:
         pygame.init()
         # Initiating Pygame Mixer
         pygame.mixer.init()
         # Declaring track Variable
-        self.track = ""
+        self.track = None
         # Declaring Status Variable
-        self.status = ""
+        self.status = "-Stopped"
 
 
-    def playsong(self):
+    def playsong(self, track):
         # Displaying Selected Song title
-        self.track.set(self.playlist.get(ACTIVE))
+        self.track = track
         # Displaying Status
-        self.status.set("-Playing")
+        self.status = "-Playing"
         # Loading Selected Song
-        pygame.mixer.music.load(self.playlist.get(ACTIVE))
+        pygame.mixer.music.load(track)
         # Playing Selected Song
         pygame.mixer.music.play()
 
     def stopsong(self):
         # Displaying Status
-        self.status.set("-Stopped")
+        self.status = "-Stopped"
         # Stopped Song
         pygame.mixer.music.stop()
 
     def pausesong(self):
         # Displaying Status
-        self.status.set("-Paused")
+        self.status = "-Paused"
         # Paused Song
         pygame.mixer.music.pause()
 
     def unpausesong(self):
         # It will Display the  Status
-        self.status.set("-Playing")
+        self.status = "-Playing"
         # Playing back Song
         pygame.mixer.music.unpause()
 
